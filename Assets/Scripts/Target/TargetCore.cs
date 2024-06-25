61c8cb599a403358c21b095549eb733a647c95f6
Ryoma Saito(Dedicated School Account) <96648305+saitocastel1900@users.noreply.github.com>
2024-06-25 14:59:23 +0900

    玉の数を減らす処理

--- Assets/Scripts/Target/TargetCore.cs
+++ Assets/Scripts/Target/TargetCore.cs
@@ -1,10 +1,22 @@
+using System;
 using UniRx;
 using UnityEngine;
+using Zenject;
 
 public class TargetCore : MonoBehaviour, IHitable
 {
     public IReactiveProperty<Vector3> IsHit => _isHit;
     private Vector3ReactiveProperty _isHit = new Vector3ReactiveProperty(Vector3.zero);
+
+    [Inject] private RemainingTargetCounterTextPresenter _remainingTargetCounter;
+
+    private void Start()
+    {
+        _isHit
+            .SkipLatestValueOnSubscribe()
+            .Subscribe(position => _remainingTargetCounter.Decrement())
+            .AddTo(this);
+    }
 
     public void Hit(Vector3 position)
     {
